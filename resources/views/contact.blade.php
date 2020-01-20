23e12d09d072c35bced56f2396b78486383a65f6
harib128 <tomshaddock@googlemail.com>
2020-01-20 18:32:55 +0100

    Move all application error messages to a centralised view include

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
   <h1>Contact</h1>
-  @if ($errors->any())
-      <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
-      </div>
-  @endif
   <form method="POST" action="{{ route('contact-form-submit') }}">
      @csrf
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
       @if (Request::is('/'))
         @include('inc.showcase')
       @endif
+
+      @include('inc.messages')
+
       <div class="row">
         <div class="col-md-8 col-lg-8">
           @yield('content')
