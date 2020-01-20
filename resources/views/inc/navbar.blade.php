b70bef443c9da35999f8d895441d4cbb071a07a0
harib128 <tomshaddock@googlemail.com>
2020-01-20 19:12:17 +0100

    Add active classes to nav items by checking the route names

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -5,17 +5,17 @@
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{route('home')}}">Home</a>
+      <li class="nav-item">
+        <a class="nav-link {{ Request::route()->getName() === 'home' ? 'active' : '' }}" href="{{route('home')}}">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{route('contact')}}">Contact</a>
+        <a class="nav-link {{ Request::route()->getName() === 'contact' ? 'active' : '' }}" href="{{route('contact')}}">Contact</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{route('about')}}">About</a>
+        <a class="nav-link {{ Request::route()->getName() === 'about'? 'active' : '' }}" href="{{route('about')}}">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="{{route('get.messages')}}">Messages</a>
+        <a class="nav-link {{ Request::route()->getName() === 'get.messages' ? 'active' : '' }}" href="{{route('get.messages')}}">Messages</a>
       </li>
     </ul>
   </div>
